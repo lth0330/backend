84b17d1ab318cfc13f63688549e1c543a37e5d77
lth0330 <dlxogud555@gmail.com>
2026-01-26 10:07:19 +0900

    0126시작

---
+++ src/day05/Exam1.java
@@ -0,0 +1,16 @@
+package day05;
+
+public class Exam1 {
+
+    public static void main(String[] args) {
+
+        String content1 = null;
+        String writer1 = null;
+        String content2 = null;
+        String content3 = null;
+        String content4 = null;
+        String content5 = null;
+
+
+    }
+}
