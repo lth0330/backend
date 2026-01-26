7e3c581aa8a7e641fa8e1bea69bfae48c1b7b7c1
lth0330 <dlxogud555@gmail.com>
2026-01-26 15:25:26 +0900

    01-26(객체 생성)

---
+++ src/day05/종합예제2.java
@@ -0,0 +1,17 @@
+package day05;
+
+public class 종합예제2 {
+    public static void main(String[] args) {
+
+
+        String [] contents = new String[100];
+        String [] writers = new String[100];
+
+        for(;;){
+            System.out.println("============My Community==============");
+            System.out.println("1. 게시물 쓰기 |  2. 게시물 출력");
+            System.out.println("======================================");
+
+        }
+    }
+}
