4be01419df296e63c8b9cd072901cd9a7d11cebb
lth0330 <dlxogud555@gmail.com>
2026-02-05 17:29:49 +0900

    2026-02-05(JDBC)

---
+++ src/Day11/Exam3.java
@@ -0,0 +1,35 @@
+package Day11;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class Exam3 {
+    public static void main(String[] args) {
+
+        //[JDBC] : 자바 데이터베이스 연동 라이브러리
+        // mysql-connector-j-9.6.0.jar
+        // 준비된 .jar 파일을 프로젝트 폴더에 넣어준다
+        // .jar 파일을 오른쪽 클릭 -> 라이브러리 추가(프로젝트 마다 1번)
+
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            String url = "jdbc:mysql://localhost:3306/mydb0205";
+            String user = "root";
+            String password = "1234";
+
+            Connection conn = DriverManager.getConnection(url,user,password);
+            System.out.println("=======연동 성공=======");
+
+            String sql = "insert into buy values(null, 'BLK', '지갑',null,30,2)"; // 실행할 sql 문법 문자열로 작성
+            PreparedStatement ps = conn.prepareStatement(sql); // 실행 메소드 사용
+            int count = ps.executeUpdate();// 기개된 sql 실행
+            System.out.println("등록된 개수는 "+count+"개 입니다.");
+        }catch (ClassNotFoundException e){
+            System.out.println("JDBC 클래스 호출 실패");
+        }catch (SQLException e){
+            System.out.println("등록된 DB 서버로 연동 실패");
+        }
+    }
+}
--- src/실습/실습13.java
+++ src/실습/실습13.java
@@ -5,9 +5,7 @@ public class 실습13 {
         // [1] 다형성특징 : 상속(+객체) vs 인터페이스( - 객체)
 
         System.out.println("==============문제 1번===============");
-
         Dog dog = new Dog();
-
         Soundable soundable = new Cat();
         soundable.makeSound();
         soundable = dog;
@@ -17,9 +15,24 @@ public class 실습13 {
         System.out.println(RemoteControl.MAX_VOLUME);;
         System.out.println(RemoteControl.Min_VOLUME);
 
-    }
-}
-/*[문제 1] 기본 인터페이스와 구현*/
+        System.out.println("==============문제 3번===============");
+        Runnable runner;
+        runner = new Person(); runner.run();
+        runner = new Car();  runner.run();
+
+        System.out.println("==============문제 4번===============");
+
+
+
+
+
+
+
+
+            }
+        }
+
+/*------------------------------[문제 1] 기본 인터페이스와 구현----------------------*/
 
 interface Soundable { public abstract void makeSound();}
 
@@ -31,12 +44,43 @@ class Cat implements Soundable {
 class Dog implements Soundable {
     public void makeSound() {
         System.out.println("멍멍.");
+
     }
 }
 
-// [문제 2] 인터페이스 상수
-
+// ----------------------------[문제 2] 인터페이스 상수------------------------
 interface RemoteControl{
     public static final int MAX_VOLUME = 10;
     public static final int Min_VOLUME = 0;
 }
+
+//---------------------[문제 3] 다형성: 인터페이스 타입 변환----------------------
+interface Runnable{
+    public abstract void run();
+}
+class Person implements Runnable{
+    public void run(){System.out.println("사람이 달립니다.");}
+}
+class Car implements Runnable{
+    public void run(){System.out.println("자동차가 달립니다.");}
+}
+
+//---------------------[문제 4] 다형성을 활용한 매개변수----------------------
+interface Attackable{
+    public abstract void attack();
+}
+class Sward implements Attackable{
+    public void attack(){
+        System.out.println("검으로 공격");
+    }
+}
+class Gun implements Attackable{
+    public void attack(){
+        System.out.println("총으로 공격");
+    }
+}
+
+
+
+
+
